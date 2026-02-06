6ff5397059d17cf9b9a3ad06dbd8c76cbeac9b7a
Sameer Al Sahab <224245762+SameerAlSahab@users.noreply.github.com>
2026-02-06 22:47:50 +0600

    mods: add dialogue booster from paradigm

--- astro/twentyfive/sound/Add-Speaker-Features.sh
+++ astro/twentyfive/sound/Add-Speaker-Features.sh
@@ -5,9 +5,29 @@
 # MOD_DESC="Add some special effects and voice booster from paradigm."
 #
 # ==============================================================================
+
 if GET_FEATURE DEVICE_HAVE_DUAL_SPEAKER; then
 
-FF "AUDIO_CONFIG_SOUNDALIVE_VERSION" "eq_custom,uhq_onoff,karaoke,adapt,spk_stereo,dvfs_20_percent,dvfs_max_45_percent,voice_boost"
+APPEND_FF_VALUE() {
+    local FEATURE_TAG="$1"
+    local APPEND_VALUE="$2"
+
+    local CURRENT_VAL
+    CURRENT_VAL=$(GET_FF_VAL "$FEATURE_TAG")
+
+
+    if [[ -z "$CURRENT_VAL" ]]; then
+        FF "$FEATURE_TAG" "$APPEND_VALUE"
+    elif [[ ",$CURRENT_VAL," == *",$APPEND_VALUE,"* ]]; then
+        LOG_INFO "Skipping appending ${APPEND_VALUE} already exists in ${FEATURE_TAG}"
+    else
+        # Append with a comma
+        local NEW_VAL="${CURRENT_VAL},${APPEND_VALUE}"
+        FF "$FEATURE_TAG" "$NEW_VAL"
+    fi
+}
+
+APPEND_FF_VALUE "AUDIO_CONFIG_SOUNDALIVE_VERSION" "voice_boost"
 
 ADD_FROM_FW "pa3q" "system" "lib64/libvoice_booster.so"
 ADD_FROM_FW "pa3q" "system" "lib64/lib_sag_ai_sound_sep_v1.00.so"
