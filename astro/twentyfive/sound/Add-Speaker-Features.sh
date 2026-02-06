98593f52ca17b6979e98f33eb4f6c28c56477b48
Sameer Al Sahab <224245762+SameerAlSahab@users.noreply.github.com>
2026-02-06 14:14:29 +0600

    mods: typo fix for sound mod

--- astro/twentyfive/sound/Add-Speaker-Features.sh
+++ astro/twentyfive/sound/Add-Speaker-Features.sh
@@ -10,7 +10,7 @@ if GET_FEATURE DEVICE_HAVE_DUAL_SPEAKER; then
 FF "AUDIO_CONFIG_SOUNDALIVE_VERSION" "eq_custom,uhq_onoff,karaoke,adapt,spk_stereo,dvfs_20_percent,dvfs_max_45_percent,voice_boost"
 
 ADD_FROM_FW "pa3q" "system" "lib64/libvoice_booster.so"
-ADD_FROM_FW "pa3q" "system" "lib64/ib_sag_ai_sound_sep_v1.00.so"
+ADD_FROM_FW "pa3q" "system" "lib64/lib_sag_ai_sound_sep_v1.00.so"
 ADD_FROM_FW "pa3q" "system" "lib64/lib_SAG_EQ_ver2060.so"
 ADD_FROM_FW "pa3q" "system" "lib64/libSAG_VM_Energy_v300.so"
 ADD_FROM_FW "pa3q" "system" "lib64/libSAG_VM_Score_V300.so"
