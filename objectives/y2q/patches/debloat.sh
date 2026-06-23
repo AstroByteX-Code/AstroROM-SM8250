2b3beab7514db34f3641760963bea2f321236e48
AstroByteX-Code <codebymianhamza@gmail.com>
2026-06-23 09:33:18 +0500

    clean-up system_ext Bloat

--- objectives/y2q/patches/debloat.sh
+++ objectives/y2q/patches/debloat.sh
@@ -33,4 +33,20 @@ SILENT REMOVE "system" "etc/permissions/privapp-permissions-com.samsung.android.
 SILENT REMOVE "system" "etc/permissions/privapp-permissions-com.samsung.petservice.xml"
 SILENT REMOVE "system" "etc/permissions/privapp-permissions-com.samsung.videoscan.xml"
 
+# system_ext clean-up
+SILENT REMOVE "system" "etc/permissions/org.carconnectivity.android.digitalkey.rangingintent.xml"	
+SILENT REMOVE "system" "etc/permissions/org.carconnectivity.android.digitalkey.secureelement.xml"
+
+BLOAT_TARGETS+=(
+    "QCC"
+	"com.qualcomm.location"
+	"com.qualcomm.qti.services.systemhelper"
+)
+
+SILENT REMOVE "system" "system_ext/bin/qccsyshal@1.2-service"
+SILENT REMOVE "system" "system_ext/etc/init/vendor.qti.hardware.qccsyshal@1.2-service.rc"
+SILENT REMOVE "system" "system_ext/etc/permissions/com.qti.location.sdk.xml"
+SILENT REMOVE "system" "system_ext/etc/permissions/com.qualcomm.location.xml"
+SILENT REMOVE "system" "system_ext/etc/permissions/privapp-permissions-com.qualcomm.location.xml"
+
 LOG_END "Debloated successfully"
